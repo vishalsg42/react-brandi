5f3fe6418437a6569adb0a59986c345549259059
guptavishals <vishalsg@prdxn.com>
2018-06-19 22:30:51 +0530

    added galler component

---
+++ src/Components/GalleryBox/GalleryBox.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const GalleryBox = (props)=> {
+  let {galleryData} = props;
+  let galleryItem = galleryData.map((item,index)=>{
+    return (
+      <div key={index} className="gallery-box">
+        <img src={item.gallery_img} alt="Kon" className="gallery-img" className="kongAnimal" />
+        <div className="overlay">
+          <div className="overlay-content">
+            <img src={props.icon} alt="Eye icon" />
+              <h3>{item.gallery_title}</h3>
+              <p>{item.gallery_description}</p>
+          </div>
+        </div>
+      </div>
+    )
+
+  });
+  return (
+        <div className="work-gallery">
+            {galleryItem}
+        </div>
+  );
+}
+
+export default GalleryBox;
