327b4c3a734e82aea8c401fc9b52723b1f981c62
guptavishals <vishalsg@prdxn.com>
2018-06-19 22:30:13 +0530

    changes for dom

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -10,13 +10,68 @@ import Toon_img from '../../assets/images/image_toon.jpg';
 import Seperator from '../../assets/images/seperator.png';
 import Icon_Eye from '../../assets/images/icon_Eye.png';
 
+import GalleryBox from '../GalleryBox/GalleryBox';
+
+const componentTitle = 'Works';
+const galleryItem = [
+  {
+    gallery_img : Kong_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  },
+  {
+    gallery_img : Stork_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  },
+  {
+    gallery_img : Militant_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  },
+  {
+    gallery_img : River_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  },
+  {
+    gallery_img : Ciel_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  },
+  {
+    gallery_img : Abstract_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  },
+  {
+    gallery_img : Toon_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  },
+  {
+    gallery_img : Drop_img,
+    gallery_title: 'Labore et dolore magnam',
+    gallery_description: 'Photography'
+  }
+]
+
+
 class Works extends Component {
+  // constructor() {
+  //   super()
+  //   // this.getClicked = this.getClicked.bind(this,'parameter')
+  // }
+  getClicked (e) {
+    e.preventDefault();
+    console.log(e.target.className);
+  };
   render() {
     return (
        // work
         <section className="work-section">
           <div className="wrapper">
-            <h2>Works</h2>
+            <h2>{componentTitle}</h2>
              <div className="feature-icon">
               <img src={Seperator} alt="featureIcon" />
             </div>
@@ -28,108 +83,27 @@ class Works extends Component {
               <option value="web">Branding</option>
               <option value="Log Design">Logo Design</option>
               <option value="Photography">Photography</option>
-
             </select>
+
             <ul className="work-nav">
               <li>
-                <a href="#FIXME" className="active" title="All">all</a>
+                <a href="#FIXME" className="active"  onClick={this.getClicked} title="All">all</a>
               </li>
               <li>
-                <a href="#FIXME" title="Branding">branding</a>
+                <a href="#FIXME" title="Branding" >branding</a>
               </li>
               <li>
-                <a href="#FIXME" title="web">web</a>
+                <a href="#FIXME" title="web" >web</a>
               </li>
               <li>
-                <a href="#FIXME" title="Logo Design">logo design</a>
+                <a href="#FIXME" title="Logo Design" >logo design</a>
               </li>
               <li>
-                <a href="#FIXME" title="Photography">photography</a>
+                <a href="#FIXME" title="Photography"  >photography</a>
               </li>
             </ul>
           </div>
-          <div className="work-gallery">
-            <div className="gallery-box">
-              <img src={Kong_img} alt="Kon" className="gallery-img" className="kongAnimal" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-            <div className="gallery-box">
-              <img src={Stork_img} className="gallery-img" alt="storkBird" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-            <div className="gallery-box">
-              <img src={River_img} className="gallery-img" alt="riverFLow" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-            <div className="gallery-box">
-              <img src={Drop_img} className="gallery-img" alt="dropOcean" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-            <div className="gallery-box">
-              <img src={Militant_img} className="gallery-img" alt="millitantArmy" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-            <div className="gallery-box">
-              <img src={Ciel_img} className="gallery-img" alt="ciel" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-            <div className="gallery-box">
-              <img src={Abstract_img} className="gallery-img" alt="abrstract" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-            <div className="gallery-box">
-              <img src={Toon_img} className="gallery-img" alt="toon" />
-              <div className="overlay">
-                <div className="overlay-content">
-                  <img src={Icon_Eye} alt="Eye icon" />
-                    <h3>Labore et dolore magnam</h3>
-                    <p>Photography</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <GalleryBox galleryData={galleryItem} icon={Icon_Eye}/>
         </section>
     );
   }
